bc2db91fb89c2e3fa678dfba1586cf359208fee0
shofegen <danik_lik1@mail.ru>
2023-09-24 18:50:20 +0500

    My homework to operator 1 is done.

--- src/Main.java
+++ src/Main.java
@@ -26,15 +26,52 @@ public class Main {
         }
 
 
-        int age1 = 7;
+        int age1 = 16;
         if (age1 >= 2 && age1 <= 6) {
             System.out.println("Если возраст человека равен " + age1 + " , то ему нужно ходить в детский сад");
-        } else {
+        } else if (age1 >= 7 && age1 <= 17){
             System.out.println("Если возраст человека равен " + age1 + " , то ему нужно ходить в школу");
         }
-        if (age1 >= 7 && age1 <= 17){
-            System.out.println("Если возраст человека равен " + age1 + " , то ему нужно ходить в детский сад");
+        else if (age1 >= 18 && age1 <= 24) {
+            System.out.println("Если возраст человека равен " + age1 + " , то его место в университете");
         } else {
-            System.out.println("Если возраст человека равен " + age1 + " , то ему нужно ходить в школу");
+            System.out.println("Если возраст человека равен " + age1 + " , то ему пора ходить на работу");
         }
-    }}
+
+
+        int age2 = 15;
+        if (age2 < 5) {
+            System.out.println("Если возраст ребенка равен " + age2 + " , то он не может кататься на аттракционе");
+        } else if (age2 >= 5 && age2 <= 14){
+            System.out.println("Если возраст ребенка равен " + age2 + " , то он может кататься только в сопровождении взрослого. Если взрослого нет, то кататься нельзя");
+        } else if (age2 >= 15){
+            System.out.println("Если возраст ребенка равен " + age2 + " , то он может кататься без сопровождения взрослого");
+        }
+
+        int numberLogged = 101;
+        byte allPassengers = 102;
+        byte seating = 60;
+        int standingPlaces = 62;
+        if (numberLogged <= 60) {
+            System.out.println("В вагоне " + (seating - numberLogged) + " сидячих мест и " + standingPlaces + " стоячих мест");
+        } else if (numberLogged >=61 && numberLogged < 102) {
+            System.out.println("В вагоне " + (allPassengers - numberLogged) + " стоячих мест");
+        } else {
+            System.out.println("Мест не осталось");
+        }
+
+
+        int one = 1;
+        int two = 2;
+        int three = 3;
+        if (one >= two && one >= three) {
+            System.out.println(one + " самое большое число");
+        } else if (one ==two && one ==three) {
+            System.out.println("Все числа равны " + one);
+        } else if (two >=one && two >= three) {
+            System.out.println(two + " самое большое число");
+        } else {
+            System.out.println(three + " самое большое число");
+        }
+           }
+    }
